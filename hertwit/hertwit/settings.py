ae78cb32a097acecb0905ef3acdbdc447e1f722a
johnobc11 <OlegarioJW@cardiff.ac.uk>
2017-01-27 15:18:59 +0000

    Add files via upload
    updated

--- hertwit/hertwit/settings.py
+++ hertwit/hertwit/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'hertwit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Htwitter',
+        'USER': 'postgres',
+        'PASSWORD': 'Password1',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
--- hertwit/hertwit/urls.py
+++ hertwit/hertwit/urls.py
@@ -4,6 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from hermt import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^tweets/', views.Htweetlist.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
 
 
