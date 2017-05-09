1d45f83e702fba5b5a5eede69708a3f8ea6b2674
johnobc11 <OlegarioJW@cardiff.ac.uk>
2017-05-09 15:27:56 +0100

    Add files via upload

--- Htweetprod2/Htweets2/Streamming.py
+++ Htweetprod2/Htweets2/Streamming.py
@@ -1,11 +1,21 @@
-# Import the necessary methods from tweepy library
+
+
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from pprint import pprint
-import io
-
 import json
+
+import sys, os, django
+
+
+sys.path.append("C:/Users/hisg316/Desktop/Htweetprod2")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Htweetprod2.settings")
+django.setup()
+
+
+from Htweets2.models import Htweets2
+from profanity import profanity
 
 # Variables that contains the user credentials to access Twitter API
 consumer_key = "dAJxDkO2T5sMzDBtuz5LN9ORw"
@@ -14,11 +24,30 @@ access_token = "800388305623785476-Q2ToEqMguIZhbdhtm9P2ODg21e8F1Ep"
 access_token_secret = "DTilA2WWlSgy9pfI6iXrGFyYB6YHX0UAIzXci0JhAvIPN"
 
 
+swear_words =['anal', 'anus', 'arse', 'ballshack', 'balls', 'bastard', 'bitch', 'biatch',
+              'bloody', 'blowjob', 'blow job', 'bollock', 'bollok', 'boner', 'boob', 'bugger',
+              'bum', 'butt', 'buttplug', 'clitoris', 'cock', 'coon', 'crap', 'cunt', 'cunts','damn',
+              'dick', 'dildo', 'dyke', 'fag', 'feck', 'fellate', 'fellatio', 'felching', 'fuck',
+              'f u c k', 'fudgepacker', 'fudge packer', 'flange', 'Goddamn', 'God damn', 'hell',
+              'homo', 'jerk', 'jizz', 'knobend', 'knobend', 'knob end', 'labia', 'lmao', 'lmfao',
+              'muff', 'nigger', 'nigga', 'penis', 'piss', 'piss', 'poop', 'prick', 'pube', 'pussy',
+              'queer', 'scrotum', 'sex', 'shit', 's hit', 'slut', 'smegma', 'spunk', 'tit', 'tosser',
+               'retard', 'retards', 'twat', 'twats', 'vagina', 'wank', 'whore', 'wtf']
+
+critical_words_train = [('website is down', 'neg'),
+                        ('website down', 'neg'),
+                        ('the site is down' , 'neg'),
+                        ('your site is down', 'neg'),
+                        ('the website is down,' 'neg')]
+#load custom bad words
+profanity.load_words(swear_words)
+
 def classificationAnalysis(text):
     # method that takes the text and spits out the argument
     classified_text = len(text)
 
     return classified_text
+
 
 
 # This is a basic listener that just prints received tweets to stdout.
@@ -35,16 +64,30 @@ class StdOutListener(StreamListener):
         print(json.loads(data))
         self.tweet_data.append(json.loads(data))
 
-        with open('htweetings1.json', 'w') as out:
-            json.dump(self.tweet_data, out)
+        tweets = Htweets2()
+        for x in self.tweet_data:
+            tweets.tweet_timestamp = x['timestamp_ms']
+            tweets.tweet_id = x['id']
+            tweets.tweet_screenname = x['user']['screen_name']
+            tweets.tweet_recount = x['retweet_count']
+            tweets.tweet_favour_count = x['favorite_count']
+            tweets.tweet_text = profanity.censor(x['text'])
+            tweets.tweet_location = x['user']['location']
+
+            if 'media' in self.tweet_data:
+                tweets.tweet_media_entities = x['entities']['media'][0]['media_url']
+
+            tweets.save()
 
     def gettext(self):
         for tweets in self.tweet_data:
             print(tweets["text"])
 
 
-def on_error(self, status):
-    print(status)
+    def on_error(self, status):
+        print(status)
+
+
 
 
 if __name__ == '__main__':
@@ -55,4 +98,4 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
 
     # This line filter Twitter Streams to capture data by the keywords: '@myhermes
-    stream.filter(track=['@myhermes'])
+    stream.filter(track=['@testH17'])
