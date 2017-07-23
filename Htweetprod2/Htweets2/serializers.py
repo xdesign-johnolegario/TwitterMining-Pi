d7c8900f9a510dbf645799f7518dabf2bca97c83
johnobc11 <OlegarioJW@cardiff.ac.uk>
2017-07-23 03:10:03 +0100

    Add files via upload

--- Htweetprod2/Htweets2/serializers.py
+++ Htweetprod2/Htweets2/serializers.py
@@ -5,4 +5,4 @@ class Htweets2Serializer(serializers.ModelSerializer):
 
     class Meta:
         model = Htweets2
-        fields = ('tweet_id', 'tweet_timestamp', 'tweet_screenname','tweet_favour_count', 'tweet_recount', 'tweet_location', 'tweet_text', 'tweet_media_entities')
+        fields = ('tweet_id', 'tweet_timestamp', 'tweet_screenname','tweet_favour_count', 'tweet_recount', 'tweet_location', 'tweet_text', 'tweet_media_entities', 'tweet_status', 'tweet_score')
